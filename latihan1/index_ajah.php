be6fac7764fdd6701853561c77b9288eb43c6e7c
RizkiIqbal02 <roxashiqbal12@gmail.com>
2023-04-09 10:05:44 +0700

    Menambahkan folder latihan1 untuk belajar dengan ayang

---
+++ latihan1/index_ajah.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rizki Iqbal Muladi</title>
+	<style>
+		
+	</style>
+</head>
+<body>
+	<h1>Nama saya iqbal</h1>
+	<h2></h2>
+	<p>kalau ini teks biasa, kalau ini bisa di <a href="https://google.com">klik</a></p>
+
+</body>
+</html>
